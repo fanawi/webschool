0d0c7a7fd932ab5f2753730de8698da00af878c1
fanawi <rulnabawi27@gmail.com>
2024-12-27 17:13:26 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+   <div class="bg- shadow-lg shadow-cyan-500/50 w-[100%]">
+      <img src="../assets/img/Logo.png" class="h-[100px]">
+   </div>
+</template>
+
+
+<style scoped>
+
+</style>
